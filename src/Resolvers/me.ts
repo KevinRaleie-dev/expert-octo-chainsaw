8b1ca4749c65da9e75515f48b5b2c05f7c18f72b
KevinRaleie-dev <kkevraleie@gmail.com>
2021-01-04 21:03:23 +0200

    adjusted me query and the type for context

--- src/Resolvers/me.ts
+++ src/Resolvers/me.ts
@@ -5,10 +5,10 @@ import { AppContext } from '../types';
 @Resolver()
 export class MeResolver {
   @Query(() => User, { nullable: true })
-  async me(@Ctx() { req }: AppContext) {
+  async me(@Ctx() { req }: AppContext): Promise<User | undefined> {
     // check if the user is logged in
     if (!req.session.userId) {
-      return null;
+      return undefined;
     }
 
     // if user is logged in display info
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from 'express';
 export interface AppContext {
   res: Response;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   req: Request & { session: any };
 }
