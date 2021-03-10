1f4164116f8eba28813363709cdc062dad3fdf27
KevinRaleie-dev <kkevraleie@gmail.com>
2021-03-10 14:07:35 +0200

    deleted some unused imports, unnecessary comments

--- src/Entity/Appointment.ts
+++ src/Entity/Appointment.ts
@@ -1,15 +1,7 @@
 import { Field, ID, ObjectType } from 'type-graphql';
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
-// import { Clinic } from "./Clinic";
-// import { Service } from "./Service"
 import { User } from './User';
 
-/**
- * there's roughly 3 relationships in this entity:
- * 1. the user thats making the appoinment
- * 2. the clinic where this appointment is taking place
- * 3. the service that the user selected for the appointment
- */
 @ObjectType()
 @Entity()
 export class Appointment {
